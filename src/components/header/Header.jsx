071d5f237e4b269ceb5a117e1c5d08a96ce1324c
Saikat Mondal <saikat23mondal.com>
2022-05-11 21:08:31 -0700

    first commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,32 @@
+import { faBed, faCar, faPlane, faTaxi } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import "./header.css"
+
+export const Header = () => {
+  return (
+    <div className="header">
+        <div className="headerlist">
+            <div className="headerListItem active">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Stays</span>
+            </div>
+            <div className="headerListItem">
+            <FontAwesomeIcon icon={faPlane} />
+            <span>Flight</span>
+            </div>
+            <div className="headerListItem">
+            <FontAwesomeIcon icon={faCar} />
+            <span>Car Rentals</span>
+            </div>
+            <div className="headerListItem">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Attractions</span>
+            </div>
+            <div className="headerListItem">
+            <FontAwesomeIcon icon={faTaxi} />
+            <span>Airport Taxi</span>
+            </div>
+        </div>
+    </div>
+  )
+}
